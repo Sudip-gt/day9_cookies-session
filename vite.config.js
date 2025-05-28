f6498cd6af1bdac49bbb042c411e6d9d9094a751
Sudip-gt <sudippaudel6561@gmail.com>
2025-05-28 17:12:24 +0545

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',        // Set project root (default is '.')
-  base: './Sudip-gt',        // Use relative paths for assets (important for deployment)
+  base: './Sudip-gt/day9_cookies-session',        // Use relative paths for assets (important for deployment)
   build: {
     outDir: 'dist',       // Output directory for build
     emptyOutDir: true,    // Clean output directory before each build
